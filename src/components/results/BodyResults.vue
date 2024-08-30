9913510c1e1a24d950135ee099e8d00901ab11d9
timlunar <lunar.tim@gmail.com>
2024-08-30 18:01:28 +0200

    Reused component to display api results

---
+++ src/components/results/BodyResults.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  temp: {
+    type: Number,
+    default: null
+  },
+  feelsLike: {
+    type: Number,
+    required: true
+  },
+  tempMin: {
+    type: Number,
+    required: true
+  },
+  tempMax: {
+    type: Number,
+    required: true
+  },
+  humidity: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <p v-if="temp">Temperature: {{ temp }}°C</p>
+  <p>Feels like: {{ feelsLike }}°C</p>
+  <p>Min: {{ tempMin }}°C</p>
+  <p>Max: {{ tempMax }}°C</p>
+  <p>Humidity: {{ humidity }}%</p>
+</template>
+
+<style scoped></style>
