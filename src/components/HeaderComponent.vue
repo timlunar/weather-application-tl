4445aa4aca171ea63437644dfba9956183107e2a
timlunar <lunar.tim@gmail.com>
2024-08-30 18:00:59 +0200

    Add header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header class="header">
+    <div class="overlay">
+      <h1>{{ title }}</h1>
+    </div>
+  </header>
+</template>
+
+<style></style>
