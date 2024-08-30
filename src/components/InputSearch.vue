c34f64b99ed44aa9e3126eef95661bce9e71d9b8
timlunar <lunar.tim@gmail.com>
2024-08-30 18:01:04 +0200

    Add input search component

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  search: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  maxLength: {
+    type: Number,
+    default: 100
+  },
+  errorMessage: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div>
+    <input
+      :value="search"
+      :type="type"
+      :placeholder="placeholder"
+      :maxlength="maxLength"
+      class="input"
+      @input="$emit('searchChanged', $event.target.value)"
+    />
+
+    <span v-if="errorMessage" class="input-error">{{ errorMessage }}</span>
+  </div>
+</template>
+
+<style scoped lang="scss" src="@/assets/scss/_input.scss" />
