56ab5c28d8f32a5fb464c3767d2c9933bb93b2bc
timlunar <lunar.tim@gmail.com>
2024-08-30 18:00:37 +0200

    Add result section component
    Component serves as wrapper for weather informational section of the app (where the results are displayed)

---
+++ src/components/ResultSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+import CurrentResults from '@/components/results/CurrentResults.vue'
+import HistoryResults from '@/components/results/HistoryResults.vue'
+
+defineProps({
+  historyResults: {
+    type: Array,
+    default: () => []
+  },
+  currentCityData: {
+    type: Object,
+    default: () => {}
+  }
+})
+</script>
+
+<template>
+  <section class="result-container">
+    <current-results :current-city-data="currentCityData" />
+
+    <history-results
+      title="History of results"
+      :history-results="historyResults"
+      @emit="$emit('displayCity', $event)"
+      @deleteHistory="$emit('deleteHistory')"
+    />
+  </section>
+</template>
+
+<style scoped></style>
